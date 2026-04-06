9e96c0fde0d282460d482147b346ae54b2d81d3e
Kavya I <24f1000013@ds.study.iitm.ac.in>
2026-04-06 11:57:41 +0530

    Add vulnerable Flask app and Docker setup

---
+++ app.py
@@ -0,0 +1,14 @@
+from flask import Flask, request
+import sqlite3
+
+app = Flask(__name__)
+
+@app.route('/search')
+def search():
+    query = request.args.get('q')
+    conn = sqlite3.connect('db.sqlite3')
+    result = conn.execute(f"SELECT * FROM users WHERE name = '{query}'")
+    return str(result.fetchall())
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=8080)
